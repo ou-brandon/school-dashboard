3cd37d313c3a7ace2b393f0d550f1e4918b4c695
julian donald <zentixzaneic@gmail.com>
2022-05-24 15:06:02 -0400

    make the error message less threatening

--- src/components/navbar/ErrorPage.js
+++ src/components/navbar/ErrorPage.js
@@ -3,8 +3,8 @@ import Typography from '@mui/material/Typography';
 const ErrorPage = props => {
 	return (
 		<>
-			<Typography variant="h4">ERROR 404</Typography>
-			<Typography variant="body">That page doesn't exist. Go back or hit one of the buttons above.</Typography>
+			<Typography variant="h4">error 404 :(</Typography>
+			<Typography variant="body1">That page doesn't exist. Go back or hit one of the buttons above.</Typography>
 		</>
 	);
 };
