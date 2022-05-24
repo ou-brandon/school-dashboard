f99d3f969df7728de90afb50d7998c3793393e95
julian donald <zentixzaneic@gmail.com>
2022-05-24 17:18:32 -0400

    display class entries

--- src/Components/classes/ClassList.js
+++ src/Components/classes/ClassList.js
@@ -1,5 +1,46 @@
-const ClassList = props => {
+import db from '../../firebase.js';
+import { collection, getDocs } from 'firebase/firestore';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { useEffect, useState } from 'react';
 
+const ClassList = props => {
+	const [classEntries, setClassEntries] = useState();
+
+	let classes = [];
+	useEffect(() => {
+		getDocs(collection(db, 'classes'))
+			.then((allDocs) => {
+				setClassEntries(allDocs);
+				});
+	}, []);
+		
+	if (classEntries)
+		classEntries.forEach((doc) => {
+			console.log(doc.data());
+			classes.push(<ClassButton info={doc} key={doc.id} />);
+		});
+
+	return (
+		<>
+			<List>
+				{classes}
+			</List>
+		</>
+	);
+};
+
+const ClassButton = props => {
+	const name = props.info.data().name;
+	return (
+		<ListItem disablePadding>
+			<ListItemButton>
+				<ListItemText primary={name} />
+			</ListItemButton>
+		</ListItem>
+	);
 };
 
 export default ClassList;
--- src/Components/classes/Classes.js
+++ src/Components/classes/Classes.js
@@ -1,12 +1,14 @@
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import ClassList from './ClassList.js';
 
 const Classes = props => {
 	return (
 		<>
+			<Typography variant="h4">Classes</Typography>
 			<Grid container spacing={2}>
 			 	<Grid item xs={2}>
-					<Typography variant="h4">Classes</Typography>
+				 	<ClassList />
 				</Grid>
 			</Grid>
 		</>
