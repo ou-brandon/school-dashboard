8f0aa6222c642ff9fafc7a24cf86d460a4628818
Matthew Haid <wsd6vn@virginia.edu>
2022-05-26 11:21:26 -0400

    Dashboard Update

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,43 +1,67 @@
-import React from 'react';
-import { Box } from '@mui/system';
-import { Typography } from '@mui/material';
-import { Grid } from '@mui/material';
-import HomepageComponent from './HomepageComponent';
-import { Card } from '@mui/material';
-import ListIcon from '@mui/icons-material/List'
-import SchoolIcon from '@mui/icons-material/School'
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import React from "react";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
+import HomepageComponent from "./HomepageComponent";
+import { Card } from "@mui/material";
+import ListIcon from "@mui/icons-material/List";
+import SchoolIcon from "@mui/icons-material/School";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import background from "./tj2.jpg";
+import styles from "./Homepage.css";
+import "./Homepage.css";
 
+// <img className="photo" src={img}></img>
 const Homepage = (props) => {
-    const studentDirectoryDescription = 'Search for students using an ID, first name, or last name. Add, edit, or delete students';
-    const teacherDirectoryDescription = 'Search for teachers using an ID, first name, or last name. Add, edit, or delete teachers';
-    const classesDescription = 'Search for classes ____. Add, edit, or delete classes';
-    const calendarDescription = 'View or add events to the school calendar ';
-    return (
-        <>
-            <Box sx={{paddingLeft: '20%', paddingRight: '20%', paddingTop: '1%'}}>
-                <Card sx={{boxShadow: 3}}>
-                    <Typography variant='h4'>Welcome to Your Dashboard!</Typography>
-                    <Grid container sx={{padding: 5}} spacing={{ xs:4, md:4}}>
-                        <Grid item xs={6}>
-                            <HomepageComponent link='/students' title='Student Directory' description={studentDirectoryDescription}/>                       
-                        </Grid>
-                        <Grid item xs={6}>
-                            <HomepageComponent link='/teachers' title='Teacher Directory' description={teacherDirectoryDescription} /> 
-                        </Grid>
-                        <Grid item xs={6}>
-                            <HomepageComponent link='/classes' title='Classes' description={classesDescription}/> 
-                        </Grid>
-                        <Grid item xs={6}>
-                            <HomepageComponent link='/calendar' title='School Calendar' description={calendarDescription}/> 
-                        </Grid>
-                    </Grid>
-                </Card>
-                
-            </Box>
-        </>
-        
-    );
-}
+  const studentDirectoryDescription =
+    "Search for students using an ID, first name, or last name. Add, edit, or delete students";
+  const teacherDirectoryDescription =
+    "Search for teachers using an ID, first name, or last name. Add, edit, or delete teachers";
+  const classesDescription =
+    "Search for classes ____. Add, edit, or delete classes";
+  const calendarDescription = "View or add events to the school calendar ";
+  return (
+    <>
+      <article
+      className="article"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      
+    </article>
+      <Box sx={{ paddingLeft: "20%", paddingRight: "20%", paddingTop: "1%" }}>
+          <Grid container direction="row" sx={{ padding: 5 }} spacing={{ xs: 4, md: 4 }}>
+            <Grid item xs={6}>
+              <HomepageComponent
+                link="/students"
+                title="Student Directory"
+                description={studentDirectoryDescription}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <HomepageComponent
+                link="/teachers"
+                title="Teacher Directory"
+                description={teacherDirectoryDescription}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <HomepageComponent
+                link="/classes"
+                title="Classes"
+                description={classesDescription}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <HomepageComponent
+                link="/calendar"
+                title="School Calendar"
+                description={calendarDescription}
+              />
+            </Grid>
+          </Grid>
+      </Box>
+    </>
+  );
+};
 
 export default Homepage;
