f7e4216bcc4cc6bd626c5dc2335f2088e2482cac
annie-sharkey <sharkeyconwaya@gmail.com>
2022-05-31 14:50:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import Navbar from './components/navbar/Navbar.js';
 import { Outlet, useLocation } from 'react-router-dom';
 import Homepage from './components/Homepage/Homepage';
 
+/*
+I would like to see functions written as an arrow function. 
+This is best practice.
+
+const App = (props if you have them ) => {
+  return (<div>stuff</div>);
+}
+*/
+
 function App() {
 
-  
+  //Your code should be as beautiful as it is functional.
+  //Don't leave random spaces like this in your code.
+  //In vscode, you can turn on formatting on save so this doesn't happen.
 
   return (
 
