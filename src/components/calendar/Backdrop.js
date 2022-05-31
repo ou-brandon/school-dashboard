1e347402aeffab5af46c44456ce0ad442a40dd71
annie-sharkey <sharkeyconwaya@gmail.com>
2022-05-31 15:12:36 -0400

    Update Backdrop.js

--- src/components/calendar/Backdrop.js
+++ src/components/calendar/Backdrop.js
@@ -1,3 +1,8 @@
+/*
+I haven't finished reading the code so I'm not sure if this gets implemented.
+If this is not implemented, remove it.
+If it is implemented, you do not need a unique file for a small amount of code like this.
+*/
 function Backdrop(props) {
     return <div className="backdrop" onClick={props.onClick} />;
   }
